cde913871e58a0223b0b570142b3630f823a4655
linyimin-bupt <linyimin520812@gmail.com>
2019-03-02 23:27:03 +0800

    implement get kth element with recursive

--- src/sort/the-kth-element.spec.ts
+++ src/sort/the-kth-element.spec.ts
@@ -1,6 +1,9 @@
 import test               from 'blue-tape'
 import { generateArray  } from './util'
-import { getElement     } from './the-kth-element'
+import { 
+  getElement,
+  getElementRecursive,
+                        } from './the-kth-element'
 
 test ('get kth element', async t => {
   const arr = generateArray (1000, 100)
@@ -16,6 +19,26 @@ test ('get kth element', async t => {
   const arr = generateArray (100000, 1000)
   console.time()
   const result = getElement (arr, 50000)
+  console.timeEnd()
+  const EXPECTED = 50000 -1
+  
+  t.equal (result, EXPECTED, 'kth element')
+})
+
+test ('get kth element recursive', async t => {
+  const arr = generateArray (1000, 100)
+  console.time()
+  const result = getElementRecursive (arr, 10)
+  console.timeEnd()
+  const EXPECTED = 10 -1
+  
+  t.equal (result, EXPECTED, 'kth element')
+})
+
+test ('get kth element recursive', async t => {
+  const arr = generateArray (100000, 1000)
+  console.time()
+  const result = getElementRecursive (arr, 50000)
   console.timeEnd()
   const EXPECTED = 50000 -1
   
--- src/sort/the-kth-element.ts
+++ src/sort/the-kth-element.ts
@@ -16,3 +16,33 @@ function swap<T> (data: T[], i: number, j: number) {
   data [i]   = data[j]
   data [j]   = temp
 }
+
+export const getElementRecursive = function<T> (data: T[], kth: number): T {
+  if (kth < 0 || kth > data.length) {
+    throw new Error('kth out of range')
+  }
+  const pivot = quickFind (data, 0, data.length - 1, kth)
+  return data[pivot]
+}
+
+function quickFind<T> (data: T[], left: number, right: number, kth: number): number {
+  const pivot = partition (data, left, right)
+  if (pivot + 1 === kth) {
+    return pivot
+  } else if (pivot + 1 < kth) {
+    return quickFind (data, pivot + 1, right, kth)
+  }
+  return quickFind (data, left, pivot - 1, kth)    
+}
+function partition<T> (data: T[], left: number, right: number): number {
+  const pivot = data[right]
+  let i = left
+  for (let j = i; j < right; j++) {
+    if (data [j] < pivot) {
+      swap (data, i, j)
+      i++
+    }
+  }
+  swap (data, i, right)
+  return i
+}
